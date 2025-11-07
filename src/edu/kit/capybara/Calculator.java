f78f3f77bf445d63eaa9806a724c09cf1cfbfdb7
Just1Developer <weisserbenjamin98@gmail.com>
2025-11-07 16:26:42 +0100

    Update Solution

---
+++ src/edu/kit/capybara/Calculator.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2025, KASTEL. All rights reserved.
+ */
+
+package edu.kit.capybara;
+
+import java.util.Scanner;
+
+/**
+ * Main class of the calculator.
+ * @author Programmieren-Team
+ */
+public final class Calculator {
+    private static final String EXIT_COMMAND_NAME = "quit";
+
+    private static final String ERROR_NO_OPERATORS = "Fehler: Du musst einen gültigen Rechenoperator benutzen";
+    private static final String ERROR_PARSE_INPUT = "Fehler: Eingabe enthält nicht das korrekte Format.%nDas korrekte Format ist: a <operator> b, bzw: \" *\\d+ *[+-*/^] *\\d+ *\"%n";
+
+    private static final char[] VALID_OPERATORS = { '+', '-', '*', '/', '^' };
+
+    private Calculator() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * The main calculator program.
+     * @param args the command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.equals(EXIT_COMMAND_NAME)) {
+                break;
+            }
+
+            int operatorIndex = -1;
+            for (char op : VALID_OPERATORS) {
+                int index = line.indexOf(op);
+                if (index != -1) {
+                    operatorIndex = index;
+                    break;
+                }
+            }
+            if (operatorIndex == -1) {
+                System.out.println(ERROR_NO_OPERATORS);
+                continue;
+            }
+            char operator = line.charAt(operatorIndex);
+
+            int leftOperand;
+            int rightOperand;
+            try {
+                leftOperand = Integer.parseInt(line.substring(0, operatorIndex).trim());
+                rightOperand = Integer.parseInt(line.substring(operatorIndex + 1).trim());
+            } catch (NumberFormatException e) {
+                System.out.printf(ERROR_PARSE_INPUT);
+                continue;
+            }
+
+            switch (operator) {
+                case '+':
+                    System.out.println(leftOperand + rightOperand);
+                    break;
+                case '-':
+                    System.out.println(leftOperand - rightOperand);
+                    break;
+                case '*':
+                    System.out.println(leftOperand * rightOperand);
+                    break;
+                case '/':
+                    System.out.println(leftOperand / rightOperand);
+                    break;
+                case '^':
+                    System.out.println(Math.pow(leftOperand, rightOperand));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // should be closed, though it's not relevant for System.in
+        scanner.close();
+    }
+}
