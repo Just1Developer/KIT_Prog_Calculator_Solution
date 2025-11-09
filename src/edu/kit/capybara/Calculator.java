633267060dd4bf7ecfe4d6a665607a5144ebd08a
Just1Developer <weisserbenjamin98@gmail.com>
2025-11-09 22:25:43 +0100

    Error

--- src/edu/kit/capybara/Calculator.java
+++ src/edu/kit/capybara/Calculator.java
@@ -14,7 +14,7 @@ public final class Calculator {
     private static final String EXIT_COMMAND_NAME = "quit";
 
     private static final String ERROR_NO_OPERATORS = "Error, du musst einen gültigen Rechenoperator benutzen";
-    private static final String ERROR_PARSE_INPUT = "Error, die Eingabe enthält nicht das korrekte Format.%nDas korrekte "
+    private static final String ERROR_PARSE_INPUT = "Error, die Eingabe enthält nicht das korrekte Format. Das korrekte "
             + "Format ist: a <operator> b, bzw: \" *\\d+ *[+-*/^] *\\d+ *\"%n";
 
     private static final char[] VALID_OPERATORS = { '+', '-', '*', '/', '^' };
